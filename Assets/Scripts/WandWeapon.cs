a0904050be1f07f04a4df2cd3feb68d434853129
Zach <zach@3blackdot.com>
2016-06-21 19:35:51 -0700

    Cleaned up WandWeapon.

--- Assets/Scripts/WandWeapon.cs
+++ Assets/Scripts/WandWeapon.cs
@@ -2,33 +2,12 @@
 using System.Collections;
 
 public class WandWeapon : MonoBehaviour {
-
-  //TODO change these to ScriptableObjects
-  //public GameObject idleParticle;
-  //public GameObject fireParticle;
-  //public GameObject lightParticle;
-
-  //public GameObject fireProjectile;
-  //public GameObject lightProjectile;
-
   public SpellData[] spellData;
-
-  //public enum SpellType
-  //{
-  //  None,
-  //  Fire,
-  //  Light
-  //}
 
   [SerializeField]
   Transform spellcastPoint;
-  //public float spellFizzleTime = 4f;
-  //public float maxSpellSize = 25f;
-  //public float scalingSpeed = 1.1f;
-  //public bool canFizzle = true;
 
   //Active OO data
-  //SpellType currentSpell;
   int currentSpell;
   GameObject currentParticleEffect;
   float spellcastTime = 0f;
@@ -67,25 +46,11 @@ public class WandWeapon : MonoBehaviour {
     currentSpell++;
     if (currentSpell > spellData.Length)
       currentSpell = 0;
-    //currentSpell = (SpellType)(((int)currentSpell + 1) % System.Enum.GetNames(typeof(SpellType)).Length);
-    //if (currentSpell == SpellType.None)
-    //{
-      //NextSpellType(); //Only want to toggle between active spells
-    //}
-    //else {
-    //if (currentParticleEffect)
-    //{
-    //  Destroy(currentParticleEffect.gameObject);
-    //}
     ResetSpell();
-    //}
   }
 
   public void StartSpellCast()
   {
-    //if (currentSpell == SpellType.None)
-    //  return;
-
     castingSpell = true;
     spellcastTime = 0f;
     totalSpellScaling = 1f;
@@ -94,18 +59,7 @@ public class WandWeapon : MonoBehaviour {
   public int RandomSpell()
   {
     currentSpell = Random.Range(0, spellData.Length);
-    //do
-    //{
-    //  currentSpell = (SpellType)Random.Range(0, System.Enum.GetNames(typeof(SpellType)).Length);
-    //  //currentSpell = (SpellType)(((int)currentSpell + 1) % System.Enum.GetNames(typeof(SpellType)).Length);
-    //} while (currentSpell == SpellType.None);
-  
-    //if (currentParticleEffect)
-    //{
-    //  Destroy(currentParticleEffect.gameObject);
-    //}
     ResetSpell();
-
     return currentSpell;
   }
 
@@ -115,28 +69,14 @@ public class WandWeapon : MonoBehaviour {
       return;
 
     GameObject spell = (GameObject)Instantiate(spellData[currentSpell].projectileObject, spellcastPoint.position, spellcastPoint.rotation);
-    //switch (currentSpell)
-    //{
-    //  case SpellType.Fire:
-    //    spell = (GameObject)Instantiate(fireProjectile, spellcastPoint.position, spellcastPoint.rotation);
-        
-    //    break;
-    //  case SpellType.Light:
-    //    spell = (GameObject)Instantiate(lightProjectile, spellcastPoint.position, spellcastPoint.rotation);
-    //    break;
-    //  default:
-
-    //    break;
-    //}
     if(spell != null)
     {
       ParticleUtilities.ScaleParticleSystem(spell, totalSpellScaling);
       spell.GetComponent<Projectile>().Activate(velocity, spellcastPoint, transform.up, totalSpellScaling);
+      spell.GetComponent<Projectile>().collisionObject = spellData[currentSpell].impactObject;
       castingSpell = false;
-      //Destroy(spell, 4f);
     }
     ResetSpell();
-    //BackToIdle();
   }
 
   void ResetSpell()
@@ -145,18 +85,6 @@ public class WandWeapon : MonoBehaviour {
       Destroy(currentParticleEffect);
 
     GameObject spellParticle = spellData[currentSpell].castingObject;
-    //switch (currentSpell)
-    //{
-    //  case SpellType.Fire:
-    //    spellParticle = fireParticle;
-    //    break;
-    //  case SpellType.Light:
-    //    spellParticle = lightParticle;
-    //    break;
-    //  default:
-    //    spellParticle = null;
-    //    break;
-    //}
     if (spellParticle != null)
     {
       currentParticleEffect = (GameObject)Instantiate(spellParticle, spellcastPoint.position, spellcastPoint.rotation);
@@ -173,18 +101,6 @@ public class WandWeapon : MonoBehaviour {
 
   public void FizzleSpell()
   {
-    //switch (currentSpell)
-    //{
-    //  case SpellType.Fire:
-    //    //TODO make spell "pop" and fade out
-    //    break;
-    //  case SpellType.Light:
-        
-    //    break;
-    //  default:
-        
-    //    break;
-    //}
     Destroy(currentParticleEffect);
     castingSpell = false;
     ResetSpell();
